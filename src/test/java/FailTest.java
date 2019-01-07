1ef2feada8fb13e92ce3ab664defa5b3627089eb
Per Nyfelt <per.nyfelt@alipsa.se>
2019-01-07 22:00:47 +0100

    cleanup

--- src/test/java/FailTest.java
+++ src/test/java/FailTest.java
@@ -1,25 +1,31 @@
+//import org.apache.log4j.Level;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 import static junit.framework.TestCase.assertTrue;
 
 public class FailTest {
 
+  /*
   static {
     System.setProperty("org.apache.commons.logging.simplelog.defaultlog", "warn");
     java.util.logging.Logger utilLog = java.util.logging.LogManager.getLogManager()
-        .getLogger(org.apache.commons.vfs2.VfsLog.class.getName());
+        .getLogger("org.apache.commons.vfs2");
     if (utilLog != null) {
       utilLog.setLevel(java.util.logging.Level.WARNING);
     }
-  }
+    org.apache.log4j.Logger log4jLog = org.apache.log4j.LogManager.getLogger(org.apache.commons.vfs2.impl.DefaultFileSystemManager.class);
+    log4jLog.setLevel(Level.WARN);
 
-  Logger log = LoggerFactory.getLogger(FailTest.class);
+  }
+  */
+
+  //Logger log = LoggerFactory.getLogger(FailTest.class);
 
   @Test
   public void testFailing() {
-    log.info("Testing something that fails");
+    System.out.println("Testing something that fails");
     assertTrue(false);
   }
 }
